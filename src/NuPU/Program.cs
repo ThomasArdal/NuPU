6d54f99b9ff0ecd7975b00845d06beb655a156de
Thomas Ardal <thomasardal@gmail.com>
2021-08-21 19:47:09 +0200

    Doc

--- src/NuPU/Program.cs
+++ src/NuPU/Program.cs
@@ -14,6 +14,12 @@ using System.Xml.Linq;
 
 namespace NuPU
 {
+    /// <summary>
+    /// Main entry point for updating NuGet packages.
+    /// Numerous lines in this file are highly inspired (some copied) from NuKeeper (https://github.com/NuKeeperDotNet/NuKeeper)
+    /// and dotnet-outdated (https://github.com/dotnet-outdated/dotnet-outdated). For much better and feature rich tools, check those out.
+    /// This is private/internal tool for now. It would be worth to consider donating it to either NuKeeper or dotnet-outdated.
+    /// </summary>
     public class Program
     {
         private const string UpToDate = " [green]up to date[/]";
